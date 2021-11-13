06babff6c99eb70873faa8d5f790536ac97e8e15
Danny McGee <dannymcgee@gmail.com>
2021-11-12 20:47:54 -0500

    Missing DebugLisp impls

--- crates/gramatika/src/debug.rs
+++ crates/gramatika/src/debug.rs
@@ -1,58 +1,63 @@
-use std::{fmt, marker::PhantomData};
+use std::{fmt, marker::PhantomData, rc::Rc, sync::Arc};
 
 pub const INDENT: &str = "  ";
 
 pub trait DebugLisp {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>, indent: usize) -> fmt::Result;
+	fn fmt(&self, f: &mut fmt::Formatter, indent: usize) -> fmt::Result;
 }
 
 // --- Foreign type impls ----------------------------------------------------------------
 
-impl<T> DebugLisp for Box<T>
-where T: DebugLisp
-{
-	fn fmt(&self, f: &mut fmt::Formatter<'_>, indent: usize) -> fmt::Result {
+impl<T: DebugLisp> DebugLisp for &T {
+	fn fmt(&self, f: &mut fmt::Formatter, indent: usize) -> fmt::Result {
+		(*self).fmt(f, indent)
+	}
+}
+
+impl<T: DebugLisp> DebugLisp for Box<T> {
+	fn fmt(&self, f: &mut fmt::Formatter, indent: usize) -> fmt::Result {
 		self.as_ref().fmt(f, indent)
 	}
 }
 
-impl<T> DebugLisp for Vec<T>
-where T: DebugLisp
-{
-	fn fmt(&self, f: &mut fmt::Formatter<'_>, indent: usize) -> fmt::Result {
-		if self.is_empty() {
-			return write!(f, "[]");
-		}
-
-		if f.alternate() {
-			writeln!(f, "[")?;
-
-			for element in self.iter() {
-				write!(f, "{}", INDENT.repeat(indent + 1))?;
-				element.fmt(f, indent + 1)?;
-				writeln!(f, ",")?;
-			}
-
-			write!(f, "{}]", INDENT.repeat(indent))
-		} else {
-			write!(f, "[")?;
-
-			for (idx, element) in self.iter().enumerate() {
-				if idx > 0 && idx != self.len() - 1 {
-					write!(f, ", ")?;
-				}
-				element.fmt(f, indent)?;
-			}
-
-			write!(f, "]")
-		}
+impl<T: DebugLisp> DebugLisp for Rc<T> {
+	fn fmt(&self, f: &mut fmt::Formatter, indent: usize) -> fmt::Result {
+		self.as_ref().fmt(f, indent)
 	}
 }
 
-impl<T> DebugLisp for Option<T>
-where T: DebugLisp
-{
-	fn fmt(&self, f: &mut fmt::Formatter<'_>, indent: usize) -> fmt::Result {
+impl<T: DebugLisp> DebugLisp for Arc<T> {
+	fn fmt(&self, f: &mut fmt::Formatter, indent: usize) -> fmt::Result {
+		self.as_ref().fmt(f, indent)
+	}
+}
+
+impl<T: DebugLisp> DebugLisp for Vec<T> {
+	fn fmt(&self, f: &mut fmt::Formatter, indent: usize) -> fmt::Result {
+		DebugLispList::new(f, indent).entries(self).finish()
+	}
+}
+
+impl<T: DebugLisp> DebugLisp for [T] {
+	fn fmt(&self, f: &mut fmt::Formatter, indent: usize) -> fmt::Result {
+		DebugLispList::new(f, indent).entries(self.iter()).finish()
+	}
+}
+
+impl<T: DebugLisp> DebugLisp for Rc<[T]> {
+	fn fmt(&self, f: &mut fmt::Formatter, indent: usize) -> fmt::Result {
+		DebugLispList::new(f, indent).entries(self.iter()).finish()
+	}
+}
+
+impl<T: DebugLisp> DebugLisp for Arc<[T]> {
+	fn fmt(&self, f: &mut fmt::Formatter, indent: usize) -> fmt::Result {
+		DebugLispList::new(f, indent).entries(self.iter()).finish()
+	}
+}
+
+impl<T: DebugLisp> DebugLisp for Option<T> {
+	fn fmt(&self, f: &mut fmt::Formatter, indent: usize) -> fmt::Result {
 		match self {
 			Some(inner) => {
 				write!(f, "Some(")?;
@@ -65,18 +70,18 @@ where T: DebugLisp
 }
 
 impl<T> DebugLisp for PhantomData<T> {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>, _: usize) -> fmt::Result {
+	fn fmt(&self, f: &mut fmt::Formatter, _: usize) -> fmt::Result {
 		f.write_str("<PhantomData>")
 	}
 }
 
 impl DebugLisp for String {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>, _: usize) -> fmt::Result {
+	fn fmt(&self, f: &mut fmt::Formatter, _: usize) -> fmt::Result {
 		<Self as fmt::Debug>::fmt(self, f)
 	}
 }
 
-// --- Helper struct ---------------------------------------------------------------------
+// --- Helpers ---------------------------------------------------------------------------
 
 pub struct DebugLispStruct<'a, 'b: 'a> {
 	fmt: &'a mut fmt::Formatter<'b>,
@@ -137,6 +142,72 @@ impl<'a, 'b: 'a> DebugLispStruct<'a, 'b> {
 				write!(self.fmt, "{})", INDENT.repeat(self.indent))
 			} else {
 				write!(self.fmt, ")")
+			}
+		});
+		self.result
+	}
+}
+
+pub struct DebugLispList<'a, 'b: 'a> {
+	fmt: &'a mut fmt::Formatter<'b>,
+	result: fmt::Result,
+	indent: usize,
+	has_entries: bool,
+}
+
+impl<'a, 'b: 'a> DebugLispList<'a, 'b> {
+	pub fn new(f: &'a mut fmt::Formatter<'b>, indent: usize) -> Self {
+		let result = write!(f, "[");
+
+		Self {
+			fmt: f,
+			result,
+			indent,
+			has_entries: false,
+		}
+	}
+
+	pub fn is_pretty(&self) -> bool {
+		self.fmt.alternate()
+	}
+
+	pub fn entry(&mut self, entry: &dyn DebugLisp) -> &mut Self {
+		self.result = self.result.and_then(|_| {
+			if self.is_pretty() {
+				writeln!(self.fmt)?;
+				write!(self.fmt, "{}", INDENT.repeat(self.indent + 1))?;
+				entry.fmt(self.fmt, self.indent + 1)?;
+				write!(self.fmt, ",")
+			} else {
+				if self.has_entries {
+					write!(self.fmt, ", ")?;
+				}
+				entry.fmt(self.fmt, self.indent + 1)
+			}
+		});
+
+		self.has_entries = true;
+		self
+	}
+
+	pub fn entries<D, I>(&mut self, entries: I) -> &mut Self
+	where
+		D: DebugLisp,
+		I: IntoIterator<Item = D>,
+	{
+		for entry in entries {
+			self.entry(&entry);
+		}
+		self
+	}
+
+	pub fn finish(&mut self) -> fmt::Result {
+		self.result = self.result.and_then(|_| {
+			if self.is_pretty() {
+				writeln!(self.fmt)?;
+				write!(self.fmt, "{}]", INDENT.repeat(self.indent))
+			} else {
+				write!(self.fmt, "]")
 			}
 		});
 		self.result
